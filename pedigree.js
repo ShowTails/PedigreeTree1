7df1bb8e2e07d5729eb17e9b9fd8d3e58301ce80
ShowTails <Showtailspedigreeapp@gmail.com>
2025-10-09 22:24:38 -0400

    Create pedigree.js

---
+++ pedigree.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const params = new URLSearchParams(window.location.search);
+  document.querySelectorAll('[data-field]').forEach(el => {
+    const field = el.getAttribute('data-field');
+    const value = params.get(field);
+    if (value) {
+      if (el.tagName === 'IMG') el.src = decodeURIComponent(value);
+      else el.textContent = decodeURIComponent(value);
+    }
+  });
+});
