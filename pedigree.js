fcaf2b8160a6f08686cbfe16d6b6bb8b7b7b3c4f
ShowTails <Showtailspedigreeapp@gmail.com>
2025-10-09 22:45:47 -0400

    Update pedigree.js

--- pedigree.js
+++ pedigree.js
@@ -1,3 +1,4 @@
+console.log("JS loaded");
 window.addEventListener('DOMContentLoaded', () => {
   const params = new URLSearchParams(window.location.search);
   document.querySelectorAll('[data-field]').forEach(el => {
